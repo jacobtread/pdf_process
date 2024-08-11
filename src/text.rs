93f9b8062223342319ce297d2fe540d0c499ea51
Jacobtread <jacobtread@gmail.com>
2024-08-11 22:22:00 +1200

    feat: text encryption handling and user facing functions

--- src/text.rs
+++ src/text.rs
@@ -1,9 +1,13 @@
 use std::process::Stdio;
 
+use futures::{stream::FuturesOrdered, TryStreamExt};
 use thiserror::Error;
 use tokio::{io::AsyncWriteExt, process::Command};
 
-use crate::shared::Password;
+use crate::{info::PdfInfo, shared::Password};
+
+/// Character that indicates the end of a page in a PDF file
+pub const PAGE_END_CHARACTER: char = '\u{c}';
 
 #[derive(Debug, Error)]
 pub enum PdfTextError {
@@ -18,6 +22,18 @@ pub enum PdfTextError {
 
     #[error("failed to get pdfinfo exit code: {0}")]
     PdfTextFailure(String),
+
+    #[error("page {0} is outside the number of available pages {1}")]
+    PageOutOfBounds(u32, u32),
+
+    #[error("page info page count is missing or invalid, pdf likely invalid")]
+    PageCountUnknown,
+
+    #[error("pdf is encrypted and no password was provided")]
+    PdfEncrypted,
+
+    #[error("file is not a pdf")]
+    NotPdfFile,
 }
 
 #[derive(Debug, Default, Clone)]
@@ -37,6 +53,100 @@ impl PdfTextArgs {
 
         out
     }
+}
+
+pub async fn text_all_pages(
+    data: &[u8],
+    info: &PdfInfo,
+    args: &PdfTextArgs,
+) -> Result<String, PdfTextError> {
+    // Check encryption
+    if info.encrypted().unwrap_or_default() && args.password.is_none() {
+        return Err(PdfTextError::PdfEncrypted);
+    }
+
+    let value = pages_text(data, args).await?;
+
+    // Strip page end characters
+    let value = value.replace(PAGE_END_CHARACTER, "\n");
+
+    Ok(value)
+}
+
+pub async fn text_all_pages_split(
+    data: &[u8],
+    info: &PdfInfo,
+    args: &PdfTextArgs,
+) -> Result<Vec<String>, PdfTextError> {
+    // Check encryption
+    if info.encrypted().unwrap_or_default() && args.password.is_none() {
+        return Err(PdfTextError::PdfEncrypted);
+    }
+
+    let out = pages_text(data, args).await?;
+
+    // Split on page ends
+    Ok(out
+        .split(PAGE_END_CHARACTER)
+        .map(|value| value.to_string())
+        .collect())
+}
+
+pub async fn text_pages(
+    data: &[u8],
+    info: &PdfInfo,
+    pages: Vec<u32>,
+    args: &PdfTextArgs,
+) -> Result<Vec<String>, PdfTextError> {
+    // Check encryption
+    if info.encrypted().unwrap_or_default() && args.password.is_none() {
+        return Err(PdfTextError::PdfEncrypted);
+    }
+
+    // Get the page count
+    let page_count = info
+        .pages()
+        .ok_or(PdfTextError::PageCountUnknown)?
+        .map_err(|_| PdfTextError::PageCountUnknown)?;
+
+    // Validate requested pages
+    for page in &pages {
+        if *page > page_count {
+            return Err(PdfTextError::PageOutOfBounds(*page, page_count));
+        }
+    }
+    // Render all the pages individually
+    pages
+        .into_iter()
+        .map(|page| page_text(data, page, args))
+        .collect::<FuturesOrdered<_>>()
+        .try_collect()
+        .await
+}
+
+pub async fn text_single_page(
+    data: &[u8],
+    info: &PdfInfo,
+    page: u32,
+    args: &PdfTextArgs,
+) -> Result<String, PdfTextError> {
+    // Check encryption
+    if info.encrypted().unwrap_or_default() && args.password.is_none() {
+        return Err(PdfTextError::PdfEncrypted);
+    }
+
+    // Get the page count
+    let page_count = info
+        .pages()
+        .ok_or(PdfTextError::PageCountUnknown)?
+        .map_err(|_| PdfTextError::PageCountUnknown)?;
+
+    // Validate chosen page
+    if page > page_count {
+        return Err(PdfTextError::PageOutOfBounds(page, page_count));
+    }
+
+    page_text(data, page, args).await
 }
 
 /// Extracts the text contents from the provided pdf file data
@@ -83,7 +193,6 @@ pub(crate) async fn pages_text(data: &[u8], args: &PdfTextArgs) -> Result<String
     }
 
     let value = String::from_utf8_lossy(&output.stdout);
-
     Ok(value.into_owned())
 }
 
@@ -140,15 +249,28 @@ pub(crate) async fn page_text(
     }
 
     let value = String::from_utf8_lossy(&output.stdout);
+    let mut value = value.to_string();
 
-    Ok(value.into_owned())
+    // Strip the page end char
+    if value.ends_with(PAGE_END_CHARACTER) {
+        value.pop();
+    }
+
+    Ok(value)
 }
 
 #[cfg(test)]
 mod test {
     use tokio::fs::read;
 
-    use crate::text::{page_text, pages_text, PdfTextArgs};
+    use crate::{
+        info::{pdf_info, PdfInfoArgs},
+        shared::{Password, Secret},
+        text::{
+            page_text, pages_text, text_all_pages, text_all_pages_split, text_pages,
+            text_single_page, PdfTextArgs, PdfTextError,
+        },
+    };
 
     /// Tests reading text from all pages
     #[tokio::test]
@@ -171,5 +293,132 @@ mod test {
         let expected = "Test page 2\n\n\u{c}";
         let text = page_text(&data, 2, &PdfTextArgs::default()).await.unwrap();
         assert_eq!(text.as_str(), expected);
+    }
+
+    /// Tests reading the text from a specific page
+    #[tokio::test]
+    async fn test_single_page() {
+        let data = read("./tests/samples/test-pdf-2-pages.pdf").await.unwrap();
+
+        let info = pdf_info(&data, &PdfInfoArgs::default()).await.unwrap();
+
+        let expected = "Test pdf with text in it\n\n";
+        let text = text_single_page(&data, &info, 1, &PdfTextArgs::default())
+            .await
+            .unwrap();
+        assert_eq!(text.as_str(), expected);
+
+        let expected = "Test page 2\n\n";
+        let text = text_single_page(&data, &info, 2, &PdfTextArgs::default())
+            .await
+            .unwrap();
+        assert_eq!(text.as_str(), expected);
+    }
+
+    /// Tests reading the text from all pages
+    #[tokio::test]
+    async fn test_all_pages() {
+        let data = read("./tests/samples/test-pdf-2-pages.pdf").await.unwrap();
+
+        let info = pdf_info(&data, &PdfInfoArgs::default()).await.unwrap();
+
+        let expected = "Test pdf with text in it\n\n\nTest page 2\n\n\n";
+        let text = text_all_pages(&data, &info, &PdfTextArgs::default())
+            .await
+            .unwrap();
+        assert_eq!(text.as_str(), expected);
+    }
+
+    /// Tests reading specific pages text
+    #[tokio::test]
+    async fn test_pages() {
+        let data = read("./tests/samples/test-pdf-2-pages.pdf").await.unwrap();
+
+        let info = pdf_info(&data, &PdfInfoArgs::default()).await.unwrap();
+
+        let expected = vec![
+            "Test pdf with text in it\n\n".to_string(),
+            "Test page 2\n\n".to_string(),
+        ];
+        let text = text_pages(&data, &info, vec![1, 2], &PdfTextArgs::default())
+            .await
+            .unwrap();
+        assert_eq!(text, expected);
+    }
+
+    /// Tests reading all pages text in split form
+    #[tokio::test]
+    async fn test_all_pages_split() {
+        let data = read("./tests/samples/test-pdf-2-pages.pdf").await.unwrap();
+
+        let info = pdf_info(&data, &PdfInfoArgs::default()).await.unwrap();
+
+        let expected = vec![
+            "Test pdf with text in it\n\n".to_string(),
+            "Test page 2\n\n".to_string(),
+            "".to_string(),
+        ];
+        let text = text_all_pages_split(&data, &info, &PdfTextArgs::default())
+            .await
+            .unwrap();
+        assert_eq!(text, expected);
+    }
+
+    /// Tests reading when the file is encrypted
+    #[tokio::test]
+    async fn test_encrypted() {
+        let data = read("./tests/samples/test-pdf-2-pages-encrypted.pdf")
+            .await
+            .unwrap();
+
+        let info_args = PdfInfoArgs {
+            password: Some(Password::User(Secret("password".to_string()))),
+        };
+
+        let info = pdf_info(&data, &info_args).await.unwrap();
+
+        let err = text_all_pages(&data, &info, &PdfTextArgs::default())
+            .await
+            .unwrap_err();
+        assert!(matches!(err, PdfTextError::PdfEncrypted));
+
+        let err = text_single_page(&data, &info, 1, &PdfTextArgs::default())
+            .await
+            .unwrap_err();
+        assert!(matches!(err, PdfTextError::PdfEncrypted));
+
+        let err = text_all_pages_split(&data, &info, &PdfTextArgs::default())
+            .await
+            .unwrap_err();
+        assert!(matches!(err, PdfTextError::PdfEncrypted));
+
+        let err = text_pages(&data, &info, vec![1, 2], &PdfTextArgs::default())
+            .await
+            .unwrap_err();
+        assert!(matches!(err, PdfTextError::PdfEncrypted));
+    }
+
+    /// Tests reading when the file is encrypted but the password
+    /// is provided
+    #[tokio::test]
+    async fn test_encrypted_with_password() {
+        let data = read("./tests/samples/test-pdf-2-pages-encrypted.pdf")
+            .await
+            .unwrap();
+
+        let info_args = PdfInfoArgs {
+            password: Some(Password::User(Secret("password".to_string()))),
+        };
+
+        let info = pdf_info(&data, &info_args).await.unwrap();
+
+        let args = PdfTextArgs {
+            password: Some(Password::User(Secret("password".to_string()))),
+        };
+
+        text_all_pages(&data, &info, &args).await.unwrap();
+        text_single_page(&data, &info, 1, &args).await.unwrap();
+        text_all_pages_split(&data, &info, &args).await.unwrap();
+        text_pages(&data, &info, vec![1, 2], &args).await.unwrap();
     }
 }
